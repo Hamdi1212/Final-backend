6b9ffcb1a4ddbb98c5befd479956efec129ddf78
Hamdi <hamditaboubi88@gmail.com>
2025-06-07 17:04:57 +0100

    Updated project with new features and improvements

--- obj/Release/net6.0/app.AssemblyInfo.cs
+++ obj/Release/net6.0/app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22968a195c1c94860a2172a4cb2a251f2796d3c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
